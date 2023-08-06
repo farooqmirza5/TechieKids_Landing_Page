cfe645d26030fd17f5f92e557771ba77e558aced
farooqmirza5 <59340702+farooqmirza5@users.noreply.github.com>
2023-08-06 16:57:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggleButton = document.getElementById('toggle-button');
+  const naviList = document.getElementById('navi-list');
+  toggleButton.addEventListener('click', () =>{
+      naviList.classList.toggle('active');
+    })
+
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
